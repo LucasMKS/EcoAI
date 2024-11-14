6b9747e51ecad1c42c91c619d0ffe483eca10356
LucasMKS <lucas.mar17ques@gmail.com>
2024-11-13 21:47:23 -0300

    Substitui seção de IA por novos componentes BlogArticles e FeatureSection; adiciona conteúdo e estrutura à página de Sustentabilidade

--- src/app/sustainability/page.js
+++ src/app/sustainability/page.js
@@ -4,17 +4,18 @@ import React from 'react'
 import HeroSection from '../../components/sustainability/HeroSection'
 import { SwipeCarousel } from '../../components/sustainability/Carousel';
 import { HoverImageLinks } from '../../components/sustainability/HoverImageLinks';
-import SustainabilityAISection from '../../components/sustainability/SustainabilityAISection';
+import BlogArticles from '../../components/sustainability/BlogArticles';
+import FeatureSection from '../../components/sustainability/FeatureSection';
 
 export default function Sustainability() {
   return (
     <div className="min-h-screen bg-green-50 ">
       <main className="pt-20">
         <HeroSection />
-        <SustainabilityAISection />
+        <BlogArticles />
+        <FeatureSection />
         <HoverImageLinks />
         <SwipeCarousel />
-        
       </main>
     </div>
   )
---
+++ src/components/sustainability/FeatureSection.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+import HeroVideoDialog from "../ui/hero-video-dialog";
+
+const features = [
+    {
+        name: 'Análise de dados complexos',
+        description:
+            'A IA facilita a análise de grandes volumes de dados climáticos, permitindo previsões mais precisas.',
+        icon: FiArrowRight,
+    },
+    {
+        name: 'Otimização de processos',
+        description:
+            'Sistemas de IA otimizam redes elétricas, agricultura e transporte, reduzindo o consumo energético e as emissões.',
+        icon: FiArrowRight,
+    },
+    {
+        name: 'Eficiência em monitoramento ambiental',
+        description:
+            'Ferramentas de IA monitoram florestas, emissões e ecossistemas, ajudando a identificar e mitigar impactos ambientais.',
+        icon: FiArrowRight,
+    },
+    {
+        name: 'Advanced security',
+        description:
+            'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+        icon: FiArrowRight,
+    },
+]
+
+export default function FeatureSection() {
+    return (
+        <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:text-center">
+                    <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+                    <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">
+                        Benefícios da IA no Contexto Climático
+                    </p>
+                    <p className="mt-6 text-lg/8 text-gray-600">
+                        Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+                        pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                    </p>
+                </div>
+                <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                    <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                        {features.map((feature) => (
+                            <div key={feature.name} className="relative pl-16">
+                                <dt className="text-base/7 font-semibold text-gray-900">
+                                    <div className="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                                        <feature.icon aria-hidden="true" className="size-6 text-white" />
+                                    </div>
+                                    {feature.name}
+                                </dt>
+                                <dd className="mt-2 text-base/7 text-gray-600">{feature.description}</dd>
+                            </div>
+                        ))}
+                    </dl>
+                </div>
+            </div>
+            <p className="text-3xl font-bold mt-12 text-center text-neutral-950"> VIDEOS </p>
+            <div className="mt-12 bg-neutral-900 p-6 rounded-lg shadow-md w-5/6 flex items-center justify-center gap-4 mx-auto">
+                <HeroVideoDialog
+                    animationStyle="from-center"
+                    videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+                    thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+                    thumbnailAlt="Hero Video"
+                />
+                <HeroVideoDialog
+                    animationStyle="from-center"
+                    videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+                    thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+                    thumbnailAlt="Hero Video"
+                />
+            </div>
+        </div>
+    )
+}
