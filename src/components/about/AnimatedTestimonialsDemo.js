0af0d9814dca86ac062d5411c4ff4bf00f216814
LucasMKS <lucas.mar17ques@gmail.com>
2024-11-26 10:24:29 -0300

    Atualiza links de currículos e substitui URLs externas por imagens locais na seção de depoimentos animados para melhorar a consistência e a performance.

--- src/components/about/AnimatedTestimonialsDemo.js
+++ src/components/about/AnimatedTestimonialsDemo.js
@@ -11,47 +11,36 @@ export function AnimatedTestimonialsDemo() {
       name: 'Gabriel Silva Galdino',
       designation: (
         <>
-          Ciência da Computação - UNA Contagem.
-          <Link href="/pdfs/GabrielGaldino.pdf" passHref target="_blank">
-            <p className="text-blue-500 underline hover:text-blue-700 transition duration-300">Resume</p>
-          </Link>
+          Ciência da Computação - UNA Contagem. <Link className="text-blue-500 underline hover:text-blue-700 transition duration-300" href="/pdfs/GabrielGaldino.pdf" passHref target="_blank"> Curriculo </Link>
         </>
       ),
-      src: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: '/images/ggaldino.png',
       quote1: 'Sou estudante de Ciência da Computação e estagiário na Prodemge, com uma grande paixão por tecnologia. Tenho interesse aprofundado em Java e experiência com diversas outras tecnologias, como Spring Boot e SQL, entre outras.',
       name1: 'Lucas Marques da Silva',
       designation1: (
         <>
-          Ciência da Computação - UNA Contagem.
-          <Link href="/pdfs/LucasMarques.pdf" passHref target="_blank">
-            <p className="text-blue-500 underline hover:text-blue-700 transition duration-300">Resume</p>
-          </Link>
+          Ciência da Computação - UNA Contagem. <Link className="text-blue-500 underline hover:text-blue-700 transition duration-300" href="/pdfs/GabrielGaldino.pdf" passHref target="_blank"> Curriculo </Link>
         </>
       ),
-      src1: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src1: '/images/lucasm.jpg',
     },
     en: {
       quote: 'I am an enthusiastic professional and diligent learner, seeking to explore and expand knowledge in new fields. Even without significant work experience, I possess a strong desire, adaptability, and motivation to learn.',
       name: 'Gabriel Silva Galdino',
       designation: (
         <>
-          Computer Science - UNA Contagem.
-          <Link href="/pdfs/GabrielGaldino.pdf" passHref target="_blank">
-            <p className="text-blue-500 underline hover:text-blue-700 transition duration-300">Resume</p>
-          </Link>
+          Computer Science - UNA Contagem. <Link className="text-blue-500 underline hover:text-blue-700 transition duration-300" href="/pdfs/GabrielGaldino.pdf" passHref target="_blank"> Resume </Link>
         </>
       ),
-      src: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src: '/images/ggaldino.png',
       quote1: 'I am a Computer Science student and an intern at Prodemge with a deep passion for technology. I have a strong interest in Java and experience with various technologies, including Spring Boot and SQL, among others.',
       name1: 'Lucas Marques da Silva',
       designation1: (
         <>
-          Computer Science - UNA Contagem. <Link href="/pdfs/LucasMarques.pdf" passHref target="_blank">
-            <p className="text-blue-500 underline hover:text-blue-700 transition duration-300">Resume</p>
-          </Link>
-        </>
+        Computer Science - UNA Contagem. <Link className="text-blue-500 underline hover:text-blue-700 transition duration-300" href="/pdfs/GabrielGaldino.pdf" passHref target="_blank"> Resume </Link>
+      </>
       ),
-      src1: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      src1: '/images/lucasm.jpg',
     },
   };
 
